7915fa479951b5ff7a6c508ab9137176934f30de
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-04 14:27:41 +0200

    Add chat backend

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const socetIo = require('socet.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socetIo(server);
+
+app.use(express.static('${__dirname}/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('${__dirname}/index.html');
+});
+
+io.on('connection', socet => {
+  // klient nasłuchuje na wiadomość wejścia do czatu
+  socet.on('join', name => {
+    // użytkownika, który pojawił się w aplikacji, zapisujemy do serwisu trzymającego listę osób w czacie
+    usersService.addUser({
+      id: socet.id,
+      name
+    });
+    // aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
+    io.emit('update', {
+      users: usersService.getAllUsers()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    usersService.removeUser(socket.id);
+    socket.broadcast.emit('update', {
+      users: usersService.getAllUsers()
+    });
+  });
+
+  socet.on('message', message => {
+    const { name } = usersService.getuserById(socet.id);
+    socet.broadcast.emit('message', {
+      text: message.text,
+      from: name
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
