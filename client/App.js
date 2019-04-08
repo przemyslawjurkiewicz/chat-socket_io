0189e3a950ec9e9a16ff51d18cd017b2a2b845e5
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-08 13:28:07 +0200

    Add App class

--- client/App.js
+++ client/App.js
@@ -3,3 +3,17 @@ import { hot } from 'react-hot-loader';
 import io from 'socket.io-client';
 
 const socket = io('/');
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            messages: [],
+            text:'',
+            name:''
+        };
+    }
+};
+
+export default hot(module)(App);
