260adc66a548dbe732573f44c2b629a80c49b638
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-09 08:34:22 +0200

    Add metods

--- client/App.js
+++ client/App.js
@@ -20,6 +20,31 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    socket.on('message', message => this.messageReceive(message));
+    socket.on('update', ({ users }) => this.chatUpdate(users));
+  }
+
+  messageReceive(message) {
+    const messages = [message, ...this.state.messages];
+    this.setState({ messages });
+  }
+
+  chatUpdate(users) {
+    this.setState({ users });
+  }
+
+  handleMessageSubmit(message) {
+    const messages = [message, ...this.state.messages];
+    this.setState({messages});
+    socket.emit('message', message);
+  }
+
+  handleUserSubmit(name) {
+    this.setState({name});
+    socket.emit('join', name);
+  }
+
   render() {
     return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
   }
